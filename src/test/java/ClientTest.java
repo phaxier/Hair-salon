cdfa9554cc46ef8f7a9ad87a7f127691b513bb76
Noel Bor <phaxier@gmail.com>
2019-05-27 19:47:05 +0300

    running test on clienttest.java

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -5,8 +5,14 @@ import static org.junit.Assert.assertEquals;
 public class ClientTest {
 
     @Test
-    public void Task_instantiatesCorrectly_true() {
+    public void Client_instantiatesCorrectly_true() {
         Client myClient = new Client("My hair style", 1);
         assertEquals(true, myClient instanceof Client);
+    }
+
+    @Test
+    public void Client_instantiatesWithDescription_String() {
+        Client myClient = new Client("My hair style", 1);
+        assertEquals("My hair style", myClient.getDescription());
     }
 }

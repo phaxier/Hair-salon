6992a4e64227f7c94df929c6433c3f01598d5245
Noel Bor <phaxier@gmail.com>
2019-05-28 10:36:13 +0300

    routing to landing page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,4 +10,20 @@ public class App {
         String layout = "templates/layout.vtl";
 
     }
+
+    get("/", (request, response)-> {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("stylists", Stylist.all());
+        model.put("template", "templates/index.vtl");
+        return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("stylists/:id/clients/new", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+        Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
+        model.put("stylist", stylist);
+        model.put("template", "templates/stylist-clients-form.vtl");
+        return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
 }

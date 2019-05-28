2ebac1dba387f6171b0d5afaa34c8f2408161e33
Noel Bor <phaxier@gmail.com>
2019-05-28 07:41:27 +0300

    adding logic to Stylist.java file and test

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -1,2 +1,50 @@
+import org.sql2o.Connection;
+
+import java.util.List;
+
 public class Stylist {
+
+    private String name;
+    private int id;
+
+    public Stylist(String name) {
+        this.name = name;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static List<Stylist> all() {
+        String sql = "SELECT id, name FROM stylists";
+        try (Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql).executeAndFetch(Stylist.class);
+        }
+    }
+
+    public List<Client> getClients() {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM clients where clientId=:id";
+            return con.createQuery(sql)
+                    .addParameter("id", this.id)
+                    .executeAndFetch(Client.class);
+        }
+    }
+
+    public void save() {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO stylists (name) VALUES (:name)";
+
+            this.id = (int) con.createQuery(sql,true)
+                    .addParameter("name", this.name)
+                    .executeUpdate()
+                    .getKey();
+        }
+
+    }
 }

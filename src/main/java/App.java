3729e53e45c27401de4f2dfee9f9a37f562aa889
Noel Bor <phaxier@gmail.com>
2019-05-28 15:59:11 +0300

    routing index.vtl and client.vtl file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,6 +9,26 @@ public class App {
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";
 
+        get("/", (request, response) -> {
+         Map<String, Object> model = new HashMap<String, Object>();
+         model.put("stylist", Stylist.all());
+         model.put("template", "templates/index.vtl");
+         return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        get("/stylists/:stylist_id/clients/:id", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            Stylist stylist = Stylist.find(Integer.parseInt(request.params(":stylist_id")));
+            Client client = Client.find(Integer.parseInt(request.params(":id")));
+            model.put("Stylist", stylist);
+            model.put("client", client);
+            model.put("template", "templates/client.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+
+
+
     }
 
     }

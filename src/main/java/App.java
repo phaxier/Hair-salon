80c8172a4cbbd18f6efc97e39fe9ec30223acbe1
Noel Bor <phaxier@gmail.com>
2019-05-28 16:03:54 +0300

    mapping client to stylist

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -26,7 +26,16 @@ public class App {
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
-
+        post("/stylists/:stylist_id/client/:id", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            Client client = Client.find(Integer.parseInt(request.params("id")));
+            String description = request.queryParams("description");
+            Stylist stylist = Stylist.find(client.getStylistId());
+            client.update(description);
+            String url = String.format("/stylists/%d/client/%d", stylist.getId(), client.getId());
+            response.redirect(url);
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
 
 
     }
